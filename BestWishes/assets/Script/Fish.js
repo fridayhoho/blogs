2a1293c9626e6826a5e5c83ee941b5585e980ec3
daguangPro <madiao@foxmail.com>
2018-02-03 16:30:43 +0800

    testCreator

---
+++ BestWishes/assets/Script/Fish.js
@@ -0,0 +1,20 @@
+var G_VAR = require("G_VAR")
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        ftype: 0; //类型
+        fspeed : 0; //速度
+        fdir: G_VAR.DIR_LEFT; //方向
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
---
+++ BestWishes/assets/Script/G_VAR.js
@@ -0,0 +1,6 @@
+var G_VAR = {
+	DIR_LEFT : 1 ;
+	DIR_RIGHT: 2 ;
+},
+
+module.exports = G_VAR;
