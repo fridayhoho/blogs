387fb3da72857116b141e31e12fa9030cf32cc66
daguangPro <madiao@foxmail.com>
2018-05-31 22:27:38 +0800

    Digit predict success!!!!

--- ML/machine-learning-ex3/ex3/predict.m
+++ ML/machine-learning-ex3/ex3/predict.m
@@ -7,12 +7,14 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+X = [ones(m, 1) X];
+
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -21,8 +23,13 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+    a2 = sigmoid( X * Theta1');
+    a2 = [ones(m, 1) a2];
+    a3 = sigmoid( a2 * Theta2');
 
+    [maxLikehood, indexs] = max(a3, [], 2);
 
+    p = indexs;
 
 
 
